1f018e0d5cd7dfd2c8c5f31c43677ed1590313da
Kyle Hsu <kylehkhsu@gmail.com>
2022-01-09 18:07:51 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='motion_planners',
+      version='0.0.0',
+      description='',
+      url='http://github.com/kylehkhsu/motion-planners',
+      author='Caelan Garrett',
+      license='MIT',
+      packages=['motion_planners'],
+      zip_safe=False
+)
